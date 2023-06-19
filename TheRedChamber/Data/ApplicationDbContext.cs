f6cc50da99c7485ace52519dee26bff6adef1ec4
vikingslord <a.haque98@yahoo.com>
2023-06-19 21:17:25 +0600

    Add project files.

---
+++ TheRedChamber/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TheRedChamber.Model;
+
+namespace TheRedChamber.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
